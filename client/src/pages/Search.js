11156e4d168545023c49fcabc620039639aff97b
Sharon Kim <sharonkim1709@gmail.com>
2020-08-08 17:56:36 -0400

    can successfully search books and not hardcode book on search

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,55 +1,66 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import Input from "../components/Input";
-import API from '../utils/API';
-import ResultsBook from '../components/ResultsBook';
+import API from "../utils/API";
+import ResultsBook from "../components/ResultsBook";
 class Search extends Component {
-    state={
-        search:"",
-        books:[]
-    }
-    componentDidMount(){
-        API.getBooks("Harry Potter").then((res)=>{
-            console.log(res.data.items)
-            this.setState({
-                books:res.data.items
-            })
-        }).catch((err)=>{
-            console.log(err)
-        })
-    }
-    handleInputChange = (event)=>{
-        const name = event.target.name;
-        const value = event.target.value;
-        this.setState({
-            [name]:value
-        })
-    }
-    handleFormSubmit=(event)=>{
-        event.preventDefault();
-        console.log("Clicked button");
-        API.getBooks(this.state.search).then(console.log(this.state.search.data))
-    }
-    render() {
-        return (
-            <>
-                <Input 
-                search={this.state.search}
-                handleInputChange={this.handleInputChange}
-                handleFormSubmit={this.handleFormSubmit}/>
-                {this.state.books.map((book,i)=>{
-                    return( 
-                        <ResultsBook 
-                    thumbnail={book.volumeInfo.imageLinks.smallThumbnail}
-                    title={book.volumeInfo.title}
-                    description={book.volumeInfo.description}
-                    author={book.volumeInfo.authors}
-                    />
-                    )
-                })}
-         
-            </>
-        );
-    }
+  state = {
+    search: "",
+    books: [],
+  };
+  componentDidMount() {
+    // API.getBooks("Harry Potter")
+    //   .then((res) => {
+    //     console.log(res.data.items);
+    //     this.setState({
+    //       books: res.data.items,
+    //     });
+    //   })
+    //   .catch((err) => {
+    //     console.log(err);
+    //   });
+  }
+  handleInputChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    this.setState({
+      [name]: value,
+    });
+  };
+  handleFormSubmit = (event) => {
+    event.preventDefault();
+    console.log("Clicked button");
+    API.getBooks(this.state.search)
+    .then((res) => {
+      console.log(res.data.items);
+      this.setState({
+        books: res.data.items,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  };
+  render() {
+    return (
+      <>
+        <Input
+          search={this.state.search}
+          handleInputChange={this.handleInputChange}
+          handleFormSubmit={this.handleFormSubmit}
+        />
+        {this.state.books.map((book, i) => {
+          return (
+            <ResultsBook
+              thumbnail={book.volumeInfo.imageLinks.smallThumbnail}
+              title={book.volumeInfo.title}
+              description={book.volumeInfo.description}
+              author={book.volumeInfo.authors}
+            />
+          );
+        })}
+      </>
+    );
+  }
 }
 
 export default Search;
