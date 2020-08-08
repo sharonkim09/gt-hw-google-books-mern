7cdb3d0850370302b0725c0f1b3ce298a7574914
Sharon Kim <sharonkim1709@gmail.com>
2020-08-08 16:50:39 -0400

    can render hardcoded book title images onto page

---
+++ client/src/components/ResultsBook.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const ResultsBook = (props) => {
+    return (
+        <>
+        <div className="row">
+        <div className="col s12 m7">
+          <div className="card">
+            <div className="card-image">
+              <img className="card.img"src={props.thumbnail}/>
+            </div>
+            <div className="card-content">
+            <p className="card-title">Card Title</p>
+              <p>I am a very simple card. I am good at containing small bits of information.
+              I am convenient because I require little markup to use effectively.</p>
+            </div>
+            <div className="card-action">
+              <button className="btn btn-dark" id="viewBtn">View Book</button>
+              <button className="btn btn-dark" id="saveBtn">Save Book</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      </>
+    );
+};
+
+export default ResultsBook;
