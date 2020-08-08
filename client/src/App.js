0f3428ce589bf07c753a1254143df2dac59c9fcd
Sharon Kim <sharonkim1709@gmail.com>
2020-08-08 12:38:16 -0400

    created header component that will show on both pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import NavBar from "./components/NavBar"
+import NavBar from "./components/NavBar";
+import Header from "./components/Header";
 // import './App.css';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 function App() {
   return (
     <Router>
       <NavBar/>
+      <Header/>
     </Router>
   );
 }
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+      <div className="container">
+          <div className="center">
+          <h1>Google Books Search</h1>
+        <h5>Search for and Save Book of Interest</h5>
+          </div>
+      </div>
+  );
+};
+
+export default Header;
