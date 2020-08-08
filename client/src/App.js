1d2cd218a705bd3e4e76fa6f9c28ad9050bff814
Sharon Kim <sharonkim1709@gmail.com>
2020-08-08 10:27:09 -0400

    created navbar component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-
+import NavBar from "./components/NavBar"
 // import './App.css';
-
+import { BrowserRouter as Router, Route } from "react-router-dom";
 function App() {
   return (
-    <h1>Hello, this is the start of google books!</h1>
+    <Router>
+      <NavBar/>
+    </Router>
   );
 }
 
