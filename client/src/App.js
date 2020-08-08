fecd39202d5e6070e84811d3a125b6116a1bfecf
Sharon Kim <sharonkim1709@gmail.com>
2020-08-08 12:43:04 -0400

    created search page and can successfully show initial content

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import NavBar from "./components/NavBar";
 import Header from "./components/Header";
+import Search from "./pages/Search";
 // import './App.css';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <Router>
       <NavBar/>
       <Header/>
+      <Route exact path="/search" component={Search}/>
     </Router>
   );
 }
