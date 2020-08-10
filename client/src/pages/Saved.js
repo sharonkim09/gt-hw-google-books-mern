253bfa66c65cbdb70b7e7dfa142778c29a009ab9
Sharon Kim <sharonkim1709@gmail.com>
2020-08-10 18:35:13 -0400

    styled saved books and added icons to buttons

--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -21,10 +21,6 @@ class Saved extends Component {
     return (
       <>
         <div>
-          <h1>(React) Google Books Search</h1>
-          <h4>Search for and Save Books of Interest</h4>
-        </div>
-        <div>
           {this.state.books.map((book) => (
             <SaveBooks
               key={book._id}
